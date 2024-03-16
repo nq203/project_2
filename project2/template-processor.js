9e040db4f227cf63909c6c039ddefc75c46133a5
nq203 <quanchun8@gmail.com>
2024-03-16 14:01:29 +0700

    commit

---
+++ project2/template-processor.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function TemplateProcessor(template) {
+  this.template = template;
+}
+
+TemplateProcessor.prototype.fillIn = function (dictionary) {
+  let filledTemplate = this.template.replace(/{{(.*?)}}/g, (match, property) => {
+    return dictionary[property] || "";
+  });
+  return filledTemplate;
+};
